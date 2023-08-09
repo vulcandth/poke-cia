bf8eb809ca7d349be24c9c71df99ad10245960d8
vulcandth <vulcandth@gmail.com>
2023-08-09 07:46:44 -0500

    Add The Unlicense to mbc30path.py

--- mbc30patch.py
+++ mbc30patch.py
@@ -1,3 +1,38 @@
+# poke-cia mbc30patch.py
+# This file is part of the poke-cia project, which provides tools
+# for repackaging Nintendo 3DS Virtual Console (VC) .cia files 
+# using built .gbc(s) and .patch(s) from the pret Pokémon Gen I/II repos.
+# This specific script is used to patch code.bin files.
+#
+# -------------------------------------
+# The Unlicense
+# 
+# This is free and unencumbered software released into the public domain.
+# 
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# 
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <http://unlicense.org/>
+# -------------------------------------
+
 import hashlib
 import os
 
